a5010aae74036c3be1a65e8bbeb278305c7a076a
alianhakim9 <alianhakim9@gmail.com>
2023-08-11 09:25:36 +0700

    belajar standard library : string

---
+++ 02-string.js
@@ -0,0 +1,12 @@
+// string instance method & properties
+const nama = "Alian Hakim";
+
+console.info(nama.length);
+console.info(nama.toLowerCase());
+console.info(nama.toUpperCase());
+console.info(nama.split(" "));
+
+const input = " input data ";
+console.info(input.trim()); // menghapus kiri kanan spasi
+console.info(input.trimEnd());
+console.info(input.trimStart());
